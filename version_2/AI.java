fa023b9b1a4a783ee53c835e095dfdbdc30be995
EmonWho <imanur.emon@northsouth.edu>
2020-11-12 22:25:30 +0600

    Author's name change

--- version_2/AI.java
+++ version_2/AI.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Md Imanur Rahman Emon
  */
 
 public interface AI {
--- version_2/DumbAI.java
+++ version_2/DumbAI.java
@@ -1,7 +1,7 @@
 /**
  * Realization of AI interface using simplistic random placement strategy.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Md Imanur Rahman Emon
  */
 
 import java.util.Random;
